da10850f20c907d85262f64363cdf0adbfbb6308
KangChanSong <thdrkdcks@naver.com>
2021-11-15 20:02:43 +0900

    프로젝트 생성 및 html 레이아웃 생성

---
+++ src/main/java/com/bake/BakeApplication.java
@@ -0,0 +1,13 @@
+package com.bake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakeApplication.class, args);
+    }
+
+}
